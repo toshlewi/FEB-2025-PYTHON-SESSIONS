ef936a848474538f656d7b4ad8e5bbb5a7305ba4
toshlewi <adelewigitz@gmail.com>
2025-03-11 11:53:55 +0300

    Intro to python

--- WEEK 1/index.py
+++ WEEK 1/index.py
@@ -1,2 +1,21 @@
 print("Hello World")
 print("I like coding")
+
+number = 10
+print(number)
+
+age = 27 # integer
+marks = 90.5 # float
+name = "Tosh lewi" # string
+
+# Additional variables
+height = 175.5 # float
+is_student = True # boolean
+hobbies = ["reading", "coding", "hiking"] # list
+favorite_language = "Python" # string
+
+# List Datatypes
+ 
+languages = ["Python", "Java", "C++", 3, 4.5, True]
+# print(languages)
+print(languages[-6])
